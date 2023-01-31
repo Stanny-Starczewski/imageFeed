274826c54def3d7f4b6ab8dfd13a0be6a9d7cdcf
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-01-31 09:34:55 -0500

    Revert "Спринт 8/14: 8 → Тема 4/5: Практика по работе с таблицей в проекте → Урок 5/5: Работаем с изображениями"
    This reverts commit e9e6acaf507c56f4fb39b5f4c3bed75ac55b7282.

--- ImagesList/ImagesListCell.swift
+++ ImagesList/ImagesListCell.swift
@@ -2,7 +2,5 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
-    @IBOutlet var cellImage: UIImageView!
-    @IBOutlet var likeButton: UIButton!
-    @IBOutlet var dateLabel: UILabel!
+    
 }
