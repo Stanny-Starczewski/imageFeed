b287f56f196fcc71f6cd9ff1783d0668da035d3f
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-02-05 21:04:40 -0500

    Спринт 9/15: 9 → Тема 1/5: Введение в тему → Урок 1/1: Задание

---
+++ ProfileView/ProfileViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet var userpick: UIImageView!
+    @IBOutlet var names: UILabel!
+    @IBOutlet var nick: UILabel!
+    @IBOutlet var invitations: UILabel!
+    @IBOutlet var exitbutton: UIButton!
+    
+    @IBAction func didTapExitButton() {
+    }
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+}
