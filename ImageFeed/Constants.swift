8c1cfbe9b77f4b5d6e0093d1ec5795ed25895e63
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-02-23 15:05:14 -0500

    Спринт 10/15: 10 → Тема 2/8: Работа с Unsplash API → Урок 3/3: Самостоятельная задача

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,10 @@
+
+import Foundation
+
+class forApiImageFeed {
+    let AccessKey = "EcpaR11Ji4Tz038kXvQV8KlBFEpB3mI5zdaBqkJ7gS0"
+    let SecretKey = "w1wk2mWN6qDOBpEr9XN5R6vE-vb9f7MKpfPHgtFaZZA"
+    let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    let AccessScope = "public+read_user+write_likes"
+    let DefaultBaseURL = URL(string: "https://api.unsplash.com/")!
+}
