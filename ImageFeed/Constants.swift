075f589bb7ffd0d564b41e8811faecb3aad9de7e
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-02-24 21:01:08 -0500

    Спринт 10/15: 10 → Тема 4/8: Авторизация: Web view → Урок 2/2: WebView: практика

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -1,10 +1,9 @@
-
 import Foundation
 
 class forApiImageFeed {
-    let AccessKey = "EcpaR11Ji4Tz038kXvQV8KlBFEpB3mI5zdaBqkJ7gS0"
-    let SecretKey = "w1wk2mWN6qDOBpEr9XN5R6vE-vb9f7MKpfPHgtFaZZA"
-    let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
-    let AccessScope = "public+read_user+write_likes"
-    let DefaultBaseURL = URL(string: "https://api.unsplash.com/")!
+    let accessKey = "EcpaR11Ji4Tz038kXvQV8KlBFEpB3mI5zdaBqkJ7gS0"
+    let secretKey = "w1wk2mWN6qDOBpEr9XN5R6vE-vb9f7MKpfPHgtFaZZA"
+    let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    let accessScope = "public+read_user+write_likes"
+    let defaultBaseURL = URL(string: "https://api.unsplash.com/")!
 }
