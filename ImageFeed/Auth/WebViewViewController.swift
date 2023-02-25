3ca59bf7a2edec9f92472e9bdc1aeaf3ab2af7c3
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-02-25 14:35:40 -0500

    Спринт 10/15: 10 → Тема 4/8: Авторизация: Web view → Урок 2/2: URLComponents

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -2,14 +2,26 @@ import UIKit
 import WebKit
 
 final class WebViewViewController: UIViewController {
-   
-    @IBOutlet weak var webView: WKWebView!
     
+    fileprivate let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+    
+    @IBOutlet weak var webView: WKWebView!
     
     @IBAction func didTapBackButton(_ sender: Any) {
     }
     
-    //    override func viewDidLoad() {
-//
-//    }
+    override func viewDidLoad() {
+        
+        var urlComponents = URLComponents(string: unsplashAuthorizeURLString)!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: accessKey),
+            URLQueryItem(name: "redirect_uri", value: redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: accessScope)
+        ]
+        let url = urlComponents.url!
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+    
 }
--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -1,9 +1,9 @@
 import Foundation
 
-class forApiImageFeed {
+
     let accessKey = "EcpaR11Ji4Tz038kXvQV8KlBFEpB3mI5zdaBqkJ7gS0"
     let secretKey = "w1wk2mWN6qDOBpEr9XN5R6vE-vb9f7MKpfPHgtFaZZA"
     let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     let accessScope = "public+read_user+write_likes"
     let defaultBaseURL = URL(string: "https://api.unsplash.com/")!
-}
+
